39530e7bd34a9d32c0704f4f3bfb41dafb547ced
Aniket-Patel-swg <patelaniket1207@gmail.com>
2024-08-25 15:17:12 +0530

    successfull jest test for mock API

--- common/tests/testAPI.test.ts
+++ common/tests/testAPI.test.ts
@@ -2,11 +2,73 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 import { routeHandler } from '../src/handler/sampleRouteHandler';
 import { Handler } from '../src/handler/sampleHandler';
 
-test('Checking if API is working or not', () =>{
-   const event : APIGatewayProxyEvent ={
-        
-   }
-   
-   const handler = new Handler()
+test('Checking if API is working or not', async () => {
+
+    // note this is an example event, which contains all the keys, you can edit as per your reqirement. 
+    const event: APIGatewayProxyEvent = {
+        body: JSON.stringify({ key: "value" }),
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer token",
+        },
+        multiValueHeaders: {
+            "Accept": ["text/html", "application/json"],
+        },
+        httpMethod: "POST",
+        isBase64Encoded: false,
+        path: "/myapi/resource",
+        pathParameters: {
+            id: "12345",
+        },
+        queryStringParameters: {
+            param1: "value1",
+        },
+        multiValueQueryStringParameters: {
+            param1: ["value1", "value2"],
+        },
+        stageVariables: {
+            stageName: "prod",
+        },
+        requestContext: {
+            accountId: "123456789012",
+            apiId: "api-id",
+            authorizer: {},
+            protocol: "HTTP/1.1",
+            httpMethod: "POST",
+            identity: {
+                accessKey: null,
+                accountId: null,
+                apiKey: null,
+                apiKeyId: null,
+                caller: null,
+                cognitoAuthenticationProvider: null,
+                cognitoAuthenticationType: null,
+                cognitoIdentityId: null,
+                cognitoIdentityPoolId: null,
+                principalOrgId: null,
+                clientCert: null,
+                sourceIp: "192.168.0.1",
+                user: null,
+                userAgent: "PostmanRuntime/7.26.8",
+                userArn: null,
+            },
+            path: "/myapi/resource",
+            stage: "prod",
+            requestId: "c4b6d6d1-0f9d-4e2a-bef2-abc123456789",
+            requestTimeEpoch: 1610489450000,
+            resourceId: "resource-id",
+            resourcePath: "/myapi/resource",
+            domainName: "api.example.com",
+            domainPrefix: "api",
+            extendedRequestId: null,
+            requestTime: "12/Jan/2021:19:30:50 +0000",
+        },
+        resource: "/myapi/resource",
+    };
+
+    const handler = new Handler();
+    const response = await handler.handle(event);
+
+    console.log("Response: ", response);
 
 })
